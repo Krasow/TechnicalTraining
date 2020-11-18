13e036c4ed75fd29ed6b48c0be24c6c47afdfc35
David Krasowska <krasowska89@gmail.com>
2020-11-18 13:52:16 -0500

    updated home

--- Technical/Technical/settings.py
+++ Technical/Technical/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'noq+!y3aqf0!^lb&@f63i@rl*9d@^fxz(x$i##r-@i*)3137=p'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = []
